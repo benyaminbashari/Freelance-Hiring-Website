fc966dbe3a9e08bbebc417bcd0b47c542a644f8e
Benyamin <benyaminbashari@gmail.com>
2020-08-23 16:45:15 -0600

    Wrong backend code removed

--- backend/src/main/java/com/mexfc/restapi/data/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mexfc.restapi.data.repository;
-
-import com.mexfc.restapi.data.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
